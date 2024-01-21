5d1daddb3edf9857a7072bd62d973da1beeb9370
Nguyen Hung Anh <127739423+k4t3pr0@users.noreply.github.com>
2024-01-21 15:34:59 +0700

    Update check-tools.sh

--- check-tools.sh
+++ check-tools.sh
@@ -2,117 +2,136 @@
 
 export CLICOLOR=1
 export LSCOLORS=GxFxCxDxBxegedabagaced
-COL_NC='\033[0m' 
+COL_NC='\033[0m'
 COL_LIGHT_YELLOW='\033[1;33m'
 INFO="[${COL_LIGHT_YELLOW}~${COL_NC}]"
 OVER="\\r\\033[K"
 
 if [[ "${AWVS_DEBUG}" == "true" ]]; then
-  set -ex
+    set -ex
 fi
 
 msg_info() {
-  printf "${INFO}  %s ${COL_LIGHT_YELLOW}...${COL_NC}" "${1}" 1>&2
-  sleep 3
+    printf "${INFO}  %s ${COL_LIGHT_YELLOW}...${COL_NC}" "${1}" 1>&2
+    sleep 3
 }
 
 msg_ok() {
-  printf "${OVER}  [\033[1;32m✓${COL_NC}]  %s\n" "${1}" 1>&2
+    printf "${OVER}  [\033[1;32m✓${COL_NC}]  %s\n" "${1}" 1>&2
 }
 
 msg_err() {
-  printf "${OVER}  [\033[1;31m✗${COL_NC}]  %s\n" "${1}" 1>&2
-  exit 1
+    printf "${OVER}  [\033[1;31m✗${COL_NC}]  %s\n" "${1}" 1>&2
+    exit 1
 }
 
 msg_over() {
-  printf "${OVER}%s" "  " 1>&2
+    printf "${OVER}%s" "  " 1>&2
 }
 
-if [[ ! -d /tmp/awvs ]]; then
-  msg_err "Awvs dict not found"
-  exit 1
+typeApp() {
+    if ! type "$1" >/dev/null 2>&1; then
+        apt-get -qq update
+        apt-get -qq install "$1"
+    fi
+}
+
+typeApp unzip
+typeApp curl
+
+LAST_VERSION="$(cat /awvs/LAST_VERSION | sed 's/ //g' 2>/dev/null)"
+
+if [[ "$LAST_VERSION" == 14.* ]]; then
+    check_zip_url="https://raw.githubusercontent.com/k4t3pr0/acunetix-latest/main/awvs14_listen.zip"
 fi
 
-if [[ -f /tmp/awvs/license_info.json ]]; then
-  if ! chmod 444 /tmp/awvs/license_info.json >/dev/null 2>&1; then
-    msg_err "Chmod license_info.json failed"
-  else
-    msg_ok "Chmod license_info.json Success! "
-  fi
+if [[ "$LAST_VERSION" == 15.* ]]; then
+    check_zip_url="https://raw.githubusercontent.com/k4t3pr0/acunetix-latest/main/awvs15_listen.zip"
 fi
 
-if [[ -f /tmp/awvs/wa_data.dat ]]; then
-  if ! chmod 444 /tmp/awvs/wa_data.dat >/dev/null 2>&1; then
-    msg_err "Chmod wa_data.dat failed"
-  else
-    msg_ok "Chmod wa_data.dat Success! "
-  fi
+if [[ -z "$check_zip_url" ]]; then
+    check_zip_url="https://raw.githubusercontent.com/k4t3pr0/acunetix-latest/main/awvs_listen.zip"
 fi
 
-if [[ -f /tmp/awvs/wvsc ]]; then
-  if ! chmod 777 /tmp/awvs/wvsc >/dev/null 2>&1; then
-    msg_err "Chmod wvsc failed"
-  else
+if [[ "$(curl -sLko /awvs/awvs_listen.zip ${check_zip_url} -w "%{http_code}")" != 200 ]]; then
+    msg_err "Download awvs_listen.zip failed"
+else
+    msg_ok "Download awvs_listen.zip Success! "
+fi
+
+if ! unzip -o /awvs/awvs_listen.zip -d /tmp/ >/dev/null 2>&1; then
+    msg_err "Unzip awvs_listen.zip failed"
+else
+    msg_ok "Unzip awvs_listen.zip Success! "
+fi
+
+if ls /tmp/{license_info.json,wa_data.dat,wvsc} 1>/dev/null 2>&1; then
+    msg_ok "All files exist"
+else
+    msg_err "At least one file does not exist"
+fi
+
+chmod 444 /tmp/{license_info.json,wa_data.dat}
+if [ $? -eq 0 ]; then
+    msg_ok "Chmod {license_info.json,wa_data.dat} Success! "
+else
+    msg_err "Chmod {license_info.json,wa_data.dat} failed"
+fi
+
+chmod 777 /tmp/wvsc
+if [ $? -eq 0 ]; then
     msg_ok "Chmod wvsc Success! "
-  fi
+else
+    msg_err "Chmod wvsc failed"
 fi
 
-if [[ -f /tmp/awvs/wa_data.dat ]]; then
-  if ! chown acunetix:acunetix /tmp/awvs/wa_data.dat >/dev/null 2>&1; then
-    msg_err "Chown wa_data.dat failed"
-  else
-    msg_ok "Chown wa_data.dat Success! "
-  fi
+chown acunetix:acunetix /tmp/{license_info.json,wa_data.dat}
+if [ $? -eq 0 ]; then
+    msg_ok "Chown {license_info.json,wa_data.dat} Success! "
+else
+    msg_err "Chown {license_info.json,wa_data.dat} failed"
 fi
 
-if [[ -f /tmp/awvs/license_info.json ]]; then
-  if ! mv /tmp/awvs/license_info.json /home/acunetix/.acunetix/data/license/ >/dev/null 2>&1; then
-    msg_err "Move license_info.json failed"
-  else
-    msg_ok "Move license_info.json Success! "
-  fi
+chattr -i /home/acunetix/.acunetix/data/license/* > /dev/null 2>&1
+rm -rf /home/acunetix/.acunetix/data/license/*
+
+if [ $? -eq 0 ]; then
+    msg_ok "rm license/* Success! "
+else
+    msg_err "chattr license/* failed"
 fi
 
-if [[ -f /tmp/awvs/wa_data.dat ]]; then
-  if ! mv /tmp/awvs/wa_data.dat /home/acunetix/.acunetix/data/license/ >/dev/null 2>&1; then
-    msg_err "Move wa_data.dat failed"
-  else
-    msg_ok "Move wa_data.dat Success! "
-  fi
+if ! mv /tmp/{license_info.json,wa_data.dat} /home/acunetix/.acunetix/data/license/ >/dev/null 2>&1; then
+    msg_err "Move {license_info.json,wa_data.dat} failed"
+else
+    msg_ok "Move {license_info.json,wa_data.dat} Success! "
 fi
 
-if [[ -f /tmp/awvs/wvsc ]]; then
-  if ! mv /tmp/awvs/wvsc /home/acunetix/.acunetix/v_*/scanner/ >/dev/null 2>&1; then
+if ! mv /tmp/wvsc /home/acunetix/.acunetix/v_*/scanner/ >/dev/null 2>&1; then
     msg_err "Move wvsc failed"
-  else
+else
     msg_ok "Move wvsc Success! "
-  fi
 fi
 
-
-if ! (echo '127.0.0.1 updates.acunetix.com' >/awvs/.hosts) >/dev/null 2>&1; then
-  msg_err "Add HOSTS.1 failed"
+chattr +i /home/acunetix/.acunetix/data/license/{license_info.json,wa_data.dat}
+if [ $? -eq 0 ]; then
+    msg_ok "chattr +i {license_info.json,wa_data.dat} Success! "
 else
-  msg_ok "Add HOSTS.1 Success! "
+    printf "${OVER}  [\033[1;31m✗${COL_NC}]  %s\n" "chattr +i {license_info.json,wa_data.dat} failed" 1>&2
 fi
 
-if ! (echo '127.0.0.1 erp.acunetix.com' >>/awvs/.hosts) >/dev/null 2>&1; then
-  msg_err "Add HOSTS.2 failed"
+for host in bxss.me erp.acunetix.com services.invicti.com api.segment.io cdn.segment.io data.pendo.io cdn.pendo.io bxss.s3.dualstack.us-west-2.amazonaws.com s3-r-w.dualstack.us-west-2.amazonaws.com; do grep -q $host /etc/hosts || {
+        echo "0.0.0.0 $host" >> /awvs/.hosts
+        echo ":: $host" >> /awvs/.hosts
+}; done
+if [ $? -eq 0 ]; then
+    msg_ok "Add HOSTS Success! "
 else
-  msg_ok "Add HOSTS.2 Success! "
+    msg_err "Add HOSTS failed"
 fi
 
-if ! (echo '127.0.0.1 telemetry.invicti.com' >>/awvs/.hosts) >/dev/null 2>&1; then
-  msg_err "Add HOSTS.3 failed"
+if ! rm -rf /awvs/awvs_listen.zip >/dev/null 2>&1; then
+    msg_err "Clean failed"
 else
-  msg_ok "Add HOSTS.3 Success! "
-fi
-
-
-
-if ! rm -rf /tmp/awvs >/dev/null 2>&1; then
-  msg_err "Clean failed"
-else
-  msg_ok "Clean Success! "
+    msg_ok "Clean Success! "
 fi
